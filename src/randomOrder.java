4efe5d462d4246bc041dfba45d2bba84de8a7ff4
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2020-12-13 19:10:59 -0600

    implementar ordenamiento sucesivo (no terminado)

--- src/randomOrder.java
+++ src/randomOrder.java
@@ -8,12 +8,10 @@ public class randomOrder {
         
         int[] cartas = new int[40];
         int orderedCard = 0;
-        int numCarta = 0;
         
         while(orderedCard < 40){
             Random r = new Random();
-            //int na = r.nextInt(20) + 1;
-            int na = numCarta;
+            int na = r.nextInt(20) + 1;
             int nvr = 0;
             
             for (int i = 0; i < 40; i++){
@@ -25,8 +23,21 @@ public class randomOrder {
                 cartas[orderedCard] = na;
                 orderedCard++;
             }
-            numCarta++;
         }
         return cartas;
     }
+    
+    public int[] orderInOrder(){
+        int[] cartas = new int[40];
+        int numCarta = 0;
+        
+        for(int i=0; i<40 ; i++){
+            for(int j=0 ; j<2 ; j++){
+                
+            }
+        }
+        
+        return cartas;
+    }
+    
 }
